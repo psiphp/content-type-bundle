5d117c486805c65a23d1108fccb39016eb0d794a
dantleech <dan.t.leech@gmail.com>
2016-10-05 13:39:55 +0100

    Fixed mapping

--- src/DependencyInjection/Compiler/AbstractRegistryPass.php
+++ src/DependencyInjection/Compiler/AbstractRegistryPass.php
@@ -33,7 +33,6 @@ abstract class AbstractRegistryPass implements CompilerPassInterface
         $registryDef = $container->getDefinition($this->registryId);
 
         foreach ($taggedIds as $serviceId => $attributes) {
-            var_dump($serviceId);
             $attributes = $attributes[0];
 
             if ($this->registerByAlias) {
